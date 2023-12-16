b5b7a53e8d77a5207186b7a6b27b1ee445bfe188
Josileudo <josileudorodrigues75@gmail.com>
2023-12-16 16:15:13 -0300

    Initial Commit

---
+++ conversionApp/conversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  conversionAppApp.swift
+//  conversionApp
+//
+//  Created by Josileudo on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct conversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
